f96a160dcabc61dfd5cf68b1cafcdc65d54557e2
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-04-28 15:00:52 -0700

    add footer component, adjust metatags and manifest.json

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-6 first">
+                        <div className="row">
+                            <div className="col-sm-4 col-lg-2">
+                                <a className="logo-footer" href="#"
+                                ><img src="./assets/logo-footer.svg" alt=" Logo Footer"
+                                    /></a>
+                            </div>
+                            <div className="col-sm-8 col-lg-10">
+                                <h5>We're in this together!</h5>
+                                <p>Support your local business.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-6 second">
+                        <h5>Who we are</h5>
+                        <p>
+                            A community initiative by
+                            <a
+                                href="https://dailyhive.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"> Daily Hive 
+                            </a> and 
+                            <a
+                                href="https://westcoastfoundation.org/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            > West Coast Foundation
+                            </a>.
+                            Small Business Profiles provided by Daily Hive, website created & maintained by West Coast Foundation and
+                            <a href="" target="_blank" rel="noopener noreferrer"
+                            > IT in Canada 
+                            </a> supported by students of
+                            <a
+                                href="https://www.comit.org/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            > Recode Your Future program
+                            </a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    );
+}
