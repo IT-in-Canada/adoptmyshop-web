cf6161a20e00044c6921cdc5aa448f67dae1b93f
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-05-05 18:21:54 -0700

    wrapping components with Fragment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Fragment } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <Fragment>
       <Header />
       <Footer />
-    </>
+    </Fragment>
   );
 }
 
