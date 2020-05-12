20b88d49d885b5ffc113b4fb9743582d347f9c44
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-05-11 18:45:59 -0700

    adding how it works section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Fragment } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HowItWorks from './components/HowItWorks';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <HowItWorks />
       <Footer />
     </Fragment>
   );
 }
-
 export default App;
