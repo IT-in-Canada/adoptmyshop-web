9036b15826992bbb3d390e1ff9c5579d4d2c8e70
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-05-26 18:57:17 -0700

    destructuring title form component

--- src/components/Form/Title/Title.js
+++ src/components/Form/Title/Title.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Title.scss';
 
-export default function Title(props) {
+export default function Title({title}) {
     return (
         <div className="row">
             <div className="col">
-                <h1 className="page-title">{props.title}</h1>
+                <h1 className="page-title">{title}</h1>
             </div>
         </div>
     );
